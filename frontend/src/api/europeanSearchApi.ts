5b19b307b1fcae8a4cd89a08e880c7a7126204b8
Hydrochilli <mainzer10@gmail.com>
2025-03-16 23:31:48 +0000

    removed keys

--- frontend/src/api/europeanSearchApi.ts
+++ frontend/src/api/europeanSearchApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const EUROPEANA_SEARCH_URL = "https://api.europeana.eu/search.json";
-const EUROPEANA_API_KEY = import.meta.env.VITE_EUROPEANA_API_KEY || "ggosewbi";
+const EUROPEANA_API_KEY = import.meta.env.VITE_EUROPEANA_API_KEY ;
 
 export interface EuropeanaSearchItem {
   id: string;
